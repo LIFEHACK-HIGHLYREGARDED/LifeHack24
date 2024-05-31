6a1c957a7763252910fb2aee03198e523932ca8e
HEEaZ <do.u.noe.imm@gmail.com>
2024-06-01 03:33:45 +0800

    Fix errors

--- app.py
+++ app.py
@@ -8,15 +8,20 @@ def hello():
 
 @app.route('/insert', methods=["POST"])
 def insert():
+	if not request.form or 'url' not in request.form:
+		return "Missing URL parameter", 400
+
 	url = request.form['url']
 	insertIntoDB(url)
-	return "Success"
+	return "Success", 200
 
 @app.route("/query", methods=["POST"])
 def query():
+	if not request.form or 'query' not in request.form:
+		return "Missing URL parameter", 400
 	qry = request.form['query']
 	result = queryDB(qry)
-	return result
+	return result, 200
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
