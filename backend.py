827406eafadc42f036cde58ff9de7634e872cb4c
jyztintan <justintanwk2001@gmail.com>
2024-05-31 11:41:43 +0800

    feat: create entry point for backend

---
+++ backend.py
@@ -0,0 +1,12 @@
+from LifeHack24.article_parsing.link_to_text import fetch_article_text
+from LifeHack24.spacy_processing.process_article import spacy_process
+
+
+def take_input(text):
+    # Assuming links start with http
+    if text.startswith('http'):
+        article_text = fetch_article_text(text)
+        print("Processing URL...")
+    else:
+        article_text = text
+    spacy_process(article_text)
