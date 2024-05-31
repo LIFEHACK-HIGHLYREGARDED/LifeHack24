85f2630c44280e3ed67cbd58b8e1ee6b50274c82
HEEaZ <do.u.noe.imm@gmail.com>
2024-06-01 03:19:27 +0800

    Reorganise routes

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def hello():
 	return "Hello World!"
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
-
-
 @app.route('/insert', methods=["POST"])
 def insert():
 	url = request.form['url']
@@ -21,3 +17,6 @@ def query():
 	qry = request.form['query']
 	result = queryDB(qry)
 	return result
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
